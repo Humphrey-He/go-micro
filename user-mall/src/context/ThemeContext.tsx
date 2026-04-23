7973b9fd41217c7ccc97b1c0f0979cafb2ec5150
hexiefeng <hexiefeng@zhuque.email>
2026-04-23 14:55:39 +0800

    feat: add login theme React context

---
+++ user-mall/src/context/ThemeContext.tsx
@@ -0,0 +1,25 @@
+// user-mall/src/context/ThemeContext.tsx
+import { createContext, useContext } from 'react'
+import { useThemeStore } from '@/stores/themeStore'
+import { themes, type LoginTheme } from '@/styles/login-themes/theme-config'
+
+interface ThemeContextType {
+  theme: LoginTheme
+  setTheme: (theme: LoginTheme) => void
+  config: (typeof themes)['vibrant']
+}
+
+export const ThemeContext = createContext<ThemeContextType>({
+  theme: 'vibrant',
+  setTheme: () => {},
+  config: themes.vibrant,
+})
+
+export const useLoginTheme = () => {
+  const store = useThemeStore()
+  return {
+    theme: store.theme,
+    setTheme: store.setTheme,
+    config: themes[store.theme],
+  }
+}
